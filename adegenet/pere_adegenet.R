7f2bb5dbb565666921f393d248dcab709affa6b6
poppynorthing <poppynorthing@gmail.com>
2024-04-12 16:48:19 -0700

    SNP

--- adegenet/pere_adegenet.R
+++ adegenet/pere_adegenet.R
@@ -7,8 +7,22 @@ library("seqinr")
 library("ggplot2")
 library("adegenet")
 
-g <- read.genepop("adegenet/populations.snps.gen")
-gg <- df2genind(g, sep= "_")
+gind <- read.genepop("adegenet/populations.snps.gen") #creates a genind object from a genopop input file
+gpop <- genind2genpop(gind) #creates a genpop object from a genind object
+
+X <- tab(gind, NA.method="mean") #replace NA's, generate a table called 'X' for PCA
+pca1 <- dudi.pca(X, scannf = F, scale = F)
+temp <- as.integer(pop(gind))
+myCol <- transp(c("blue","red"),.7)[temp]
+myPch <- c(15,17)[temp]
+
+plot(pca1$li, col=myCol, cex=3, pch=myPch)
+
+abline(h=0,v=0,col="grey",lty=2)
+s.arrow(pca1$c1*.5, add.plot=TRUE)
+legend("topright", pch=c(15,17), col=transp(c("blue","red"),.7),
+       leg=c("Group A","Group B"), pt.cex=2)
+
 
 pop(g) = sapply(strsplit(indNames(g), "_"), function(g){g[1]})
 
